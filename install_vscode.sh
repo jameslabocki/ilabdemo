7dad827e0a8fd1facb006b36c355fcfac85486a4
James Labocki <jlabocki@redhat.com>
2024-06-28 15:56:52 -0400

    Update install_vscode.sh

--- install_vscode.sh
+++ install_vscode.sh
@@ -3,3 +3,8 @@
 curl -L -o files/vscode.rpm 'https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64'
 
 sudo rpm -ivh files/vscode.rpm
+
+
+sudo dnf install gcc glibc-devel zlib-devel 
+sdk install maven
+sdk install quarkus
