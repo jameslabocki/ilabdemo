c5542414ada6ca5648c522186b335ac67b4916ea
James Labocki <jlabocki@redhat.com>
2024-07-09 14:40:28 -0400

    Update install_podmandesktop.sh

--- install_podmandesktop.sh
+++ install_podmandesktop.sh
@@ -4,3 +4,4 @@ flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flath
 
 flatpak install --user flathub io.podman_desktop.PodmanDesktop
 
+# https://github.com/flatpak/flatpak/issues/5488
